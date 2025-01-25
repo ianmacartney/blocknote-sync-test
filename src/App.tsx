cfa4a423667d5bc1999fe11ae7a158e1165cc158
Ian Macartney <ianhmacartney@gmail.com>
2025-01-24 17:40:48 -0800

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ import { useState } from "react";
 import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/shadcn";
 import "@blocknote/shadcn/style.css";
-import { useCreateBlockNote } from "@blocknote/react";
 
 import { useBlockNoteSync } from "@convex-dev/prosemirror-sync/blocknote";
 import { api } from "../convex/_generated/api";
-import { BlockNoteEditor, nodeToBlock } from "@blocknote/core";
 
 export default function App() {
   const [viewer] = useState(randomName());
@@ -24,40 +22,5 @@ export default function App() {
         {sync.editor && <BlockNoteView editor={sync.editor} />}
       </div>
     </Layout>
-  );
-}
-
-// const defaultSchema = BlockNoteSchema.create();
-const defaultEditor = BlockNoteEditor.create({
-  _headless: true,
-});
-
-function Doc(props: { initialContent: any; syncExtension: any }) {
-  const node = defaultEditor.pmSchema.nodeFromJSON(props.initialContent);
-  const blocks: any[] = [];
-
-  // note, this code is similar to editor.document
-  node.firstChild!.descendants((node) => {
-    blocks.push(
-      nodeToBlock(
-        node,
-        defaultEditor.schema.blockSchema,
-        defaultEditor.schema.inlineContentSchema,
-        defaultEditor.schema.styleSchema,
-      ),
-    );
-    return false;
-  });
-  const editor = useCreateBlockNote({
-    initialContent: blocks,
-    _tiptapOptions: {
-      extensions: [props.syncExtension],
-    },
-  });
-
-  return (
-    <div style={{ height: "100%", overflow: "auto" }}>
-      <BlockNoteView editor={editor} />
-    </div>
   );
 }
